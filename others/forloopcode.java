fe53a4d4bfb0fca701e7b8d8d7c4c24192755d29
anurag1602 <anuragpunewar1602@gmail.com>
2025-07-09 23:11:35 +0530

    some changes in code

--- others/forloopcode.java
+++ others/forloopcode.java
@@ -1,12 +1,16 @@
+import java.util.Scanner;
+
 public class forloopcode {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the no to print table of: ");
+        int num = sc.nextInt();
         for(int i=10;i>=1;i--){
-        table(i);
+        table(i, num);
     }
 }
 
-    public static void table(int i){
-        int table = 8*i;
-        System.out.println(8 +"*"+ i+ "="+table);
+    public static void table(int i,int num){
+        System.out.println(num +"*"+ i+ "="+num * i);
     }
 }
